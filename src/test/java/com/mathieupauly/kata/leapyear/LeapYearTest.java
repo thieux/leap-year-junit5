4abe52cdb11c0f1d17cc57aeef5ff60d5edfbad9
thieux <mathieu.pauly@gmail.com>
2018-11-06 22:14:24 +0100

    Fail test: 2000 -> true

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
@@ -11,7 +11,7 @@ class LeapYearTest {
 
     @ParameterizedTest
     @CsvSource({"1996, true, yearShouldBeLeapWhenDivisibleBy4", "2001, false, yearShouldBeCommonWhenNotDivisibleBy4",
-            "1900, false, yearShouldBeAtypicallyCommonWhenDivisibleBy100"})
+            "1900, false, yearShouldBeAtypicallyCommonWhenDivisibleBy100", "2000, true, yearShouldBeAtypicallyLeapWhenDivisibleBy400"})
     void test(int year, boolean shouldBeLeap, String description) {
         assertThat(leapYear.isLeap(year)).as(description).isEqualTo(shouldBeLeap);
     }
