bcb6160df60fb59b809493b1900a581244c66cca
thieux <mathieu.pauly@gmail.com>
2018-11-06 21:18:54 +0100

    Fail outer loop test: revolution & calendar should converge after 10,000 years

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
@@ -7,9 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LeapYearTest {
 
+    private static final int ERA_IN_YEARS = 10_000;
+    private static final double GREGORIAN_AVERAGE_YEAR_LENGTH_IN_DAYS = 365.2425;
+
     @Test
-    void test() {
-        assertThat(365.2424).isEqualTo(365.2425, Offset.offset(0.0001));
+    void delta_between_earth_revolution_and_basic_calendar_should_be_small() {
+        double eraInDays;
+        eraInDays = 365 * ERA_IN_YEARS;
+        assertThat(eraInDays).isEqualTo(GREGORIAN_AVERAGE_YEAR_LENGTH_IN_DAYS * ERA_IN_YEARS,
+                Offset.offset(0.0001));
     }
 
 }
