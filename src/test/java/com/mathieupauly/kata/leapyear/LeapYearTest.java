73d72732e74b4ce17b46c9db416628a7031a2de1
thieux <mathieu.pauly@gmail.com>
2018-11-06 21:11:13 +0100

    Show that 365.2424 != 365.2425 with the chosen offset

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
@@ -1,5 +1,6 @@
 package com.mathieupauly.kata.leapyear;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,7 +9,7 @@ class LeapYearTest {
 
     @Test
     void test() {
-        assertThat(365.2425).isEqualTo(365.2425);
+        assertThat(365.2424).isEqualTo(365.2425, Offset.offset(0.0001));
     }
 
 }
