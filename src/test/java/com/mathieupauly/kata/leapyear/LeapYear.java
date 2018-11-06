c54086e84ef26a803c4735ca21664625e5ddeb89
thieux <mathieu.pauly@gmail.com>
2018-11-06 21:56:08 +0100

    Pass test: 1900 -> false (enhance convergence from delta > 70 to delta < 30)

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYear.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYear.java
@@ -3,6 +3,9 @@ package com.mathieupauly.kata.leapyear;
 public class LeapYear {
 
     boolean isLeap(int year) {
+        if (year % 100 == 0) {
+            return false;
+        }
         return year % 4 == 0;
     }
 }
