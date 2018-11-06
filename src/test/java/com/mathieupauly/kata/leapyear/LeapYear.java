249e3cfcf65c476e552a2d4d42b26355e280f0d9
thieux <mathieu.pauly@gmail.com>
2018-11-06 21:24:51 +0100

    Move isLeap() to a dedicated class

---
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYear.java
@@ -0,0 +1,8 @@
+package com.mathieupauly.kata.leapyear;
+
+public class LeapYear {
+
+    boolean isLeap(int year) {
+        return false;
+    }
+}
--- src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
@@ -9,19 +9,16 @@ class LeapYearTest {
 
     private static final int ERA_IN_YEARS = 10_000;
     private static final double GREGORIAN_AVERAGE_YEAR_LENGTH_IN_DAYS = 365.2425;
+    private final LeapYear leapYear = new LeapYear();
 
     @Test
     void delta_between_earth_revolution_and_basic_calendar_should_be_small() {
         double eraInDays = 0;
         for (int year = 0; year < ERA_IN_YEARS; year++) {
-            eraInDays += isLeap(year) ? 366 : 365;
+            eraInDays += leapYear.isLeap(year) ? 366 : 365;
         }
         assertThat(eraInDays).isEqualTo(GREGORIAN_AVERAGE_YEAR_LENGTH_IN_DAYS * ERA_IN_YEARS,
                 Offset.offset(0.0001));
-    }
-
-    private boolean isLeap(int year) {
-        return false;
     }
 
 }
