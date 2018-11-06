a5eeff4e8a0285ccfac76723ff4265e63192061f
thieux <mathieu.pauly@gmail.com>
2018-11-06 22:15:03 +0100

    Pass test: 2000 -> true

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYear.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYear.java
@@ -3,6 +3,9 @@ package com.mathieupauly.kata.leapyear;
 public class LeapYear {
 
     boolean isLeap(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
         if (year % 100 == 0) {
             return false;
         }
