e0f8c233601df7014296e2b919f8dea681f2aa24
thieux <mathieu.pauly@gmail.com>
2018-11-06 21:04:53 +0100

    Check assertj

--- src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/mathieupauly/kata/leapyear/LeapYearTest.java
@@ -2,11 +2,13 @@ package com.mathieupauly.kata.leapyear;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LeapYearTest {
 
     @Test
     void test() {
-
+        assertThat(0).isEqualTo(1);
     }
 
 }
